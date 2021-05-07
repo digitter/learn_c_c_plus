0f720bc2b81ec246f6c043523cbaa9422aeaf2ff
kazuki <matchingserver@gmail.com>
2021-05-07 23:26:54 +0900

    learn calculation

---
+++ calculation.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%d + %d = %d\n", 5, 2, 5 + 2);
+  printf("%d - %d = %d\n", 5, 2, 5 - 2);
+  printf("%d * %d = %d\n", 5, 2, 5 * 2);
+  printf("%d / %d = %d\n", 5, 2, 5 / 2);
+}
